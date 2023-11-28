2e71ecc8d1d25d6505751a41058e777dd16fcb12
subhro1530 <shaswata.ssaha@gmail.com>
2023-11-29 01:26:17 +0530

    small changes

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,14 @@
 
 import { ChakraProvider } from "@chakra-ui/react";
 import Navbar from "../components/Navbar";
+import HeroSection from "@/components/HeroSection";
 
 const Home = () => {
   return (
     <ChakraProvider>
       <div style={{ fontFamily: "Work Sans, sans-serif" }}>
         <Navbar />
+        <HeroSection />
       </div>
     </ChakraProvider>
   );
