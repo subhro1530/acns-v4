1a010f1cd64f88b30f4963ca5eb9a66c6f669f45
Pratham Agarwal <gargpratham71@gmail.com>
2023-12-02 13:49:10 +0530

    About Hello Fixed

--- components/About.js
+++ components/About.js
@@ -15,6 +15,7 @@ const About = () => {
   const [currentLanguageIndex, setCurrentLanguageIndex] = useState(0);
   const [isVisible, setIsVisible] = useState(false);
   const [isDone, setIsDone] = useState(false);
+  const [isLast, setIsLast] = useState(false);
   const boxRef = useRef(null);
 
   useEffect(() => {
